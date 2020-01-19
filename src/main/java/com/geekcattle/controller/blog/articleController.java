11dfd5475e0e62f9eaaf97e3be2fafd69dfc5949
Admin <905051408@qq.com>
2020-01-19 16:39:38 +0800

    获取文章列表展示功能完成

--- src/main/java/com/geekcattle/controller/blog/articleController.java
+++ src/main/java/com/geekcattle/controller/blog/articleController.java
@@ -3,6 +3,7 @@ package com.geekcattle.controller.blog;
 import com.geekcattle.mapper.blog.LifeShareMapper;
 import com.geekcattle.model.blog.LifeShare;
 import com.geekcattle.util.ReturnUtil;
+import org.apache.commons.collections.map.HashedMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/artilce")
@@ -23,12 +25,32 @@ public class articleController {
     public ModelMap getAlitleDta(HttpServletRequest request) {
         try {
             List<LifeShare> lifeShareList = lifeShareMapper.selectAllData();
+            return ReturnUtil.Success(null, lifeShareList.size(), null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ReturnUtil.Error(null, null, null);
+        }
+    }
+
+    //todo 获取文章标题数据
+    @RequestMapping(value = "/getPagingAllData", method = RequestMethod.GET)
+    public ModelMap getPagingAllData(HttpServletRequest request) {
+        try {
+            int currPage = Integer.parseInt(request.getParameter("startPage")); //从第几个数据开始
+            int pageSize = Integer.parseInt(request.getParameter("endPade")); //查询多少个数据
+
+            Map<String, Object> data = new HashedMap();
+            data.put("currIndex", (currPage-1)*pageSize);
+            data.put("pageSize", pageSize);
+            List<LifeShare> lifeShareList = lifeShareMapper.selectPagingAllData(data);
             return ReturnUtil.Success(null, lifeShareList, null);
         } catch (Exception e) {
             e.printStackTrace();
             return ReturnUtil.Error(null, null, null);
         }
     }
+
+
     //todo 获取文章标题数据
     @RequestMapping(value = "/aitlecontent", method = RequestMethod.GET)
     public ModelMap getAitlecontent(HttpServletRequest request) {
--- src/main/java/com/geekcattle/mapper/blog/LifeShareMapper.java
+++ src/main/java/com/geekcattle/mapper/blog/LifeShareMapper.java
@@ -6,8 +6,11 @@ import com.geekcattle.util.CustomerMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+
 @Service
 public interface LifeShareMapper extends CustomerMapper<LifeShare> {
     List<LifeShare> selectAllData();
+    List<LifeShare> selectPagingAllData(Map<String,Object> data);
     LifeShare selectAllDataById(String id);
 }
